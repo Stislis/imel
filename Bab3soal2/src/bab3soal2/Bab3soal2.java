aae6098d004a51f251bf671c760c639f782c7518
Stislis <stislislisz@gmail.com>
2024-08-29 09:48:11 +0700

    Add files via upload

---
+++ Bab3soal2/src/bab3soal2/Bab3soal2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Bab3Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int NIM = 2110117000;                    
+        String namaMahasiswa = "Andika Jaya Utama";     
+        String kelas = "01SIFE020";                
+        int usia = 20;                         
+        double tinggiBadan = 1.72;
+        
+        System.out.println("Biodata Diri Mahasiswa");
+        System.out.println("-------------------------");
+        System.out.println("NIM             : " + NIM);
+        System.out.println("Nama Mahasiswa  : " + namaMahasiswa);
+        System.out.println("Kelas           : " + kelas);
+        System.out.println("Usia            : " + usia + " tahun");
+        System.out.println("Tinggi Badan    : " + tinggiBadan + " cm");
+    }
+}
---
+++ bab4soal1/src/bab4soal1/Bab4soal1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab4soal1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Bab4Soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 3;
+        
+        System.out.println("bilangan1 + bilangan2: " + 
+        (angka1 + angka2) );
+        System.out.println("bilangan1 - bilangan2: " + 
+        (angka1 - angka2) );
+        System.out.println("bilangan1 * bilangan2: " + 
+        (angka1 * angka2) );
+        System.out.println("bilangan1 / bilangan2: " + 
+        (angka1 / angka2) );
+    }
+}
